388192c0987fd2cf26424b4e8d096dcda0e3f34f
WilHunter <80978014+WilHunter@users.noreply.github.com>
2022-03-16 17:55:28 -0300

    Index V1.1.3 - CORS

--- index.js
+++ index.js
@@ -5,16 +5,15 @@ const port = process.env.PORT || 3000
 const app = express()
 
 app.use(express.json())
-app.use((req, res, next) =>{
-    res.header('Access-Control-Allow-Origin', '*');
-    res.header('Access-Control-Allow-Headers', 'Authorization, X-API-KEY, Origin, X-Requested-With, Content-Type, Accept, Access-Control-Allow-Request-Method');
-    res.header('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, DELETE');
-    res.header('Allow', 'GET, POST, OPTIONS, PUT, DELETE');
-    app.use(cors());
-    next();
-})
+
+app.use(cors({
+    origin: '*',
+    header: 'Authorization, X-API-KEY, Origin, X-Requested-With, Content-Type, Accept, Access-Control-Allow-Request-Method',
+    methods: 'GET, POST, OPTIONS, PUT, DELETE'
+}))
+
 app.use(routes)
 
 app.listen(port, () => {
     console.log(`Experss started http://localhost:5000`)
-  })
+})
