986277d420c23fb589684fa614f34b484b946da1
WilHunter <80978014+WilHunter@users.noreply.github.com>
2022-03-17 10:40:41 -0300

    Delete teste.env.js

--- teste.env.js
+++
@@ -1,8 +0,0 @@
-
-module.exports = {
-    USER_MAIL: 'wildocpm@hotmail.com',
-    USER_PASS: 'wilbru88',
-    SEND_URL: 'https://api.sendgrid.com/v3/mail/send',
-    SENDGRID_API_KEY: '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',
-    ID_TEMPLATE_SEGURADORA: 'd-5b005fb781894afab37cc136f669e632'
-}
